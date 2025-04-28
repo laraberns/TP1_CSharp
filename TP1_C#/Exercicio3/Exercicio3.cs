3fac29472270f81c5f42dabf45fa163f88fce1a5
Lara Pereira <larabernspereira@gmail.com>
2025-04-28 20:18:12 -0300

    add exer 3

---
+++ TP1_C#/Exercicio3/Exercicio3.cs
@@ -0,0 +1,24 @@
+﻿namespace TP1_C_.Exercicio3
+{
+    internal class Exercicio3
+    {
+        public static double CalcularBase(double altura, double largura)
+        {
+            return altura * largura;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha a base de um retângulo: ");
+            double base_retangulo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha a altura de um retângulo: ");
+            double altura_retangulo = double.Parse(Console.ReadLine());
+
+            Func<double, double, double> calcularArea = CalcularBase;
+
+            double area = calcularArea(altura_retangulo, base_retangulo);
+
+            Console.WriteLine($"A área do retângulo é: {area}");
+        }
+    }
+}
