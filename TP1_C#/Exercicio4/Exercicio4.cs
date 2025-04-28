4f287c3f79fbd998e048fad0b6b4b808294f6767
Lara Pereira <larabernspereira@gmail.com>
2025-04-28 20:45:26 -0300

    add exer 4

---
+++ TP1_C#/Exercicio4/Exercicio4.cs
@@ -0,0 +1,22 @@
+﻿namespace TP1_C_.Exercicio4
+{
+    internal class Exercicio4
+    {
+        static void Main(string[] args)
+        {
+            TemperatureSensor sensor = new TemperatureSensor();
+
+            sensor.TemperatureExceeded += Sensor_TemperatureExceeded;
+
+            Console.WriteLine("Digite a temperatura:");
+            double temperatura = double.Parse(Console.ReadLine());
+
+            sensor.ReadTemperature(temperatura);
+        }
+
+        private static void Sensor_TemperatureExceeded(object sender, EventArgs e)
+        {
+            Console.WriteLine("Temperatura acima do limite seguro!");
+        }
+    }
+}
---
+++ TP1_C#/Exercicio4/TemperatureSensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_C_.Exercicio4
+{
+    internal class TemperatureSensor
+    {
+        public event EventHandler TemperatureExceeded;
+
+        public void ReadTemperature(double temperature)
+        {
+            if (temperature > 100)
+            {
+                OnTemperatureExceeded(EventArgs.Empty);
+            }
+        }
+        protected virtual void OnTemperatureExceeded(EventArgs e)
+        {
+            TemperatureExceeded?.Invoke(this, e);
+        }
+    }
+}
